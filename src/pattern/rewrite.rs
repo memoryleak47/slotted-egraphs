811a5ba94e2487dd43d65b86db013741269446f5
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-05 22:36:43 +0200

    simplify rewrite system

--- src/pattern/rewrite.rs
+++ src/pattern/rewrite.rs
@@ -6,6 +6,11 @@ pub struct RewriteT<L: Language, T: Any> {
     pub applier: Box<dyn Fn(T, &mut EGraph<L>)>,
 }
 
+pub struct Rewrite<L: Language> {
+    pub searcher: Box<dyn Fn(&EGraph<L>) -> Box<dyn Any>>,
+    pub applier: Box<dyn Fn(Box<dyn Any>, &mut EGraph<L>)>,
+}
+
 impl<L: Language + 'static, T: 'static> RewriteT<L, T> {
     pub fn into(self) -> Rewrite<L> {
         let searcher = self.searcher;
@@ -17,67 +22,39 @@ impl<L: Language + 'static, T: 'static> RewriteT<L, T> {
     }
 }
 
-pub type Rewrite<L> = RewriteT<L, Box<dyn Any>>;
-
 fn any_to_t<T: Any>(t: Box<dyn Any>) -> T {
     *t.downcast().unwrap()
 }
 
-pub fn do_rewrites<L: Language>(eg: &mut EGraph<L>, rewrites: &[Rewrite<L>]) {
+pub fn apply_rewrites<L: Language>(eg: &mut EGraph<L>, rewrites: &[Rewrite<L>]) {
     let ts: Vec<Box<dyn Any>> = rewrites.iter().map(|rw| (*rw.searcher)(eg)).collect();
     for (rw, t) in rewrites.iter().zip(ts.into_iter()) {
         (*rw.applier)(t, eg);
     }
 }
 
-// Indirect rewrites.
-
-pub fn mk_named_rewrite_if<L: Language + 'static>(rule: &str, a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool + 'static) -> Rewrite<L> {
-    let rule = rule.to_string();
-    let a2 = a.clone();
-    RewriteT {
-        searcher: Box::new(move |eg| {
-            let x: Vec<Subst> = ematch_all(eg, &a);
-            x
-        }),
-        applier: Box::new(move |substs, eg| {
-            for subst in substs {
-                if cond(&subst) {
-                    eg.union_instantiations(&a2, &b, &subst, Some(rule.to_string()));
+impl<L: Language + 'static> Rewrite<L> {
+    pub fn new_if(rule: &str, a: &str, b: &str, cond: impl Fn(&Subst) -> bool + 'static) -> Self {
+        let a = Pattern::parse(a).unwrap();
+        let b = Pattern::parse(b).unwrap();
+        let rule = rule.to_string();
+        let a2 = a.clone();
+        RewriteT {
+            searcher: Box::new(move |eg| {
+                let x: Vec<Subst> = ematch_all(eg, &a);
+                x
+            }),
+            applier: Box::new(move |substs, eg| {
+                for subst in substs {
+                    if cond(&subst) {
+                        eg.union_instantiations(&a2, &b, &subst, Some(rule.to_string()));
+                    }
                 }
-            }
-        }),
-    }.into()
-}
+            }),
+        }.into()
+    }
 
-pub fn mk_rewrite_if<L: Language + 'static>(a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool + 'static) -> Rewrite<L> {
-    mk_named_rewrite_if("<no rule name>", a, b, cond)
-}
-
-pub fn mk_named_rewrite<L: Language + 'static>(rule: &str, a: Pattern<L>, b: Pattern<L>) -> Rewrite<L> {
-    mk_named_rewrite_if(rule, a, b, |_| true)
-}
-
-pub fn mk_rewrite<L: Language + 'static>(a: Pattern<L>, b: Pattern<L>) -> Rewrite<L> {
-    mk_rewrite_if(a, b, |_| true)
-}
-
-// Direct rewrites.
-
-pub fn rewrite_if<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool) {
-    for subst in ematch_all(eg, &a) {
-        if cond(&subst) {
-            eg.union_instantiations(&a, &b, &subst, None);
-        }
+    pub fn new(rule: &str, a: &str, b: &str) -> Self {
+        Self::new_if(rule, a, b, |_| true)
     }
 }
-
-pub fn rewrite<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
-    rewrite_if(eg, a, b, |_| true);
-}
-
-pub fn rewrite_bi<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
-    rewrite(eg, a.clone(), b.clone());
-    rewrite(eg, b, a);
-}
-
