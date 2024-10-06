e7a18b12f3f87edbeccfff3674e2c7228cc0dc6d
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-06 16:18:02 +0200

    ...

--- tests/entry.rs
+++ tests/entry.rs
@@ -6,11 +6,33 @@ pub use symbol_table::GlobalSymbol as Symbol;
 mod lamcalc;
 pub use lamcalc::*;
 
-mod langs;
-pub use langs::*;
-
 mod small;
 pub use small::*;
+
+mod arith;
+pub use arith::*;
+
+mod array;
+pub use array::*;
+
+mod lambda;
+pub use lambda::*;
+
+mod lambda_let;
+pub use lambda_let::*;
+
+mod rise;
+pub use rise::*;
+
+mod symbol;
+pub use symbol::*;
+
+mod var;
+pub use var::*;
+
+mod fgh;
+pub use fgh::*;
+
 
 pub fn singleton_set<T: Eq + Hash>(t: T) -> HashSet<T> {
     [t].into_iter().collect()
