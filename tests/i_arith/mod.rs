4129cdd86ef7539265f6b85c8459145a804922bb
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-06 13:59:55 +0200

    ...

--- tests/i_arith/mod.rs
+++ tests/i_arith/mod.rs
@@ -113,8 +113,8 @@ impl Language for ArithENode {
             ArithENode::App(l, r) => (String::from("app"), vec![Child::AppliedId(l), Child::AppliedId(r)]),
             ArithENode::Var(s) => (String::from("var"), vec![Child::Slot(s)]),
             ArithENode::Let(s, t, b) => (String::from("let"), vec![Child::Slot(s), Child::AppliedId(t), Child::AppliedId(b)]),
-            ArithENode::Number(n) => (format!("num_{}", n), vec![]),
-            ArithENode::Symbol(s) => (format!("sym_{}", s), vec![]),
+            ArithENode::Number(n) => (format!("{}", n), vec![]),
+            ArithENode::Symbol(s) => (format!("{}", s), vec![]),
             ArithENode::Add(l, r) => (String::from("add"), vec![Child::AppliedId(l), Child::AppliedId(r)]),
             ArithENode::Mul(l, r) => (String::from("mul"), vec![Child::AppliedId(l), Child::AppliedId(r)]),
         }
@@ -128,13 +128,13 @@ impl Language for ArithENode {
             ("let", [Child::Slot(s), Child::AppliedId(t), Child::AppliedId(b)]) => Some(ArithENode::Let(*s, t.clone(), b.clone())),
             ("add", [Child::AppliedId(l), Child::AppliedId(r)]) => Some(ArithENode::Add(l.clone(), r.clone())),
             ("mul", [Child::AppliedId(l), Child::AppliedId(r)]) => Some(ArithENode::Mul(l.clone(), r.clone())),
-            (op, []) if op.starts_with("num_") => {
-                let u: u32 = op[4..].parse().ok()?;
-                Some(ArithENode::Number(u))
-            },
-            (op, []) if op.starts_with("sym_") => {
-                let s: Symbol = op[4..].parse().ok()?;
-                Some(ArithENode::Symbol(s))
+            (op, []) => {
+                if let Ok(u) = op.parse::<u32>() {
+                    Some(ArithENode::Number(u))
+                } else {
+                    let s: Symbol = op.parse().ok()?;
+                    Some(ArithENode::Symbol(s))
+                }
             },
             _ => None,
         }
