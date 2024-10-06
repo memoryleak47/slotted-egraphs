3d7cc4bcf7bc0f412163ae82a72a447b077dadbc
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-06 13:35:00 +0200

    ...

--- tests/i_rise/tst.rs
+++ tests/i_rise/tst.rs
@@ -1,10 +1,11 @@
 use crate::*;
 use crate::i_rise::build::*;
 
-fn assert_reaches(start: RecExpr<RiseENode>, goal: RecExpr<RiseENode>, steps: usize) {
+fn assert_reaches(start: &str, goal: &str, steps: usize) {
+    let start = RecExpr::parse(start).unwrap();
+    let goal = RecExpr::parse(goal).unwrap();
+
     let rules = rise_rules(SubstMethod::SmallStep);
-    dbg!(&start);
-    dbg!(&goal);
 
     let mut eg = EGraph::new();
     let i1 = eg.add_expr(start.clone());
@@ -29,8 +30,6 @@ fn assert_reaches(start: RecExpr<RiseENode>, goal: RecExpr<RiseENode>, steps: us
 fn test_reduction() {
     let a = "(app (lam s0 (app (lam s1 (app (app (var s0) (var s1)) (app (app (var s0) (var s1)) (app (app (var s0) (var s1)) (app (app (var s0) (var s1)) (app (app (var s0) (var s1)) (app (app (var s0) (var s1)) (var s1)))))))) (lam s2 (app (app sym_add (var s2)) num_1)))) (lam s3 (lam s4 (lam s5 (app (var s3) (app (var s4) (var s5)))))))";
     let b = "(lam s0 (app (app sym_add (app (app sym_add (app (app sym_add (app (app sym_add (app (app sym_add (app (app sym_add (app (app sym_add (var s0)) num_1)) num_1)) num_1)) num_1)) num_1)) num_1)) num_1))";
-    let a = RecExpr::parse(a).unwrap();
-    let b = RecExpr::parse(b).unwrap();
     assert_reaches(a, b, 40);
 }
 
@@ -38,46 +37,12 @@ fn test_reduction() {
 fn test_fission() {
     let a = "(app sym_map (lam s42 (app sym_f5 (app sym_f4 (app sym_f3 (app sym_f2 (app sym_f1 (var s42))))))))";
     let b = "(lam s1 (app (app sym_map (lam s42 (app sym_f5 (app sym_f4 (app sym_f3 (var s42)))))) (app (app sym_map (lam s42 (app sym_f2 (app sym_f1 (var s42))))) (var s1))))";
-    let a = RecExpr::parse(a).unwrap();
-    let b = RecExpr::parse(b).unwrap();
     assert_reaches(a, b, 40);
 }
 
-// BINOMIAL //
-
-fn binomial_re1() -> RecExpr<RiseENode> {
-    let nbh = 0;
-    let dt = dot2(
-            join1(symb("weights2d")),
-            join1(var(nbh)));
-    let out = map2(
-        map1(lam(nbh, dt)),
-        map2(transpose0(),
-            slide3(num(3), num(1), map2(slide2(num(3), num(1)), symb("input")))
-        )
-    );
-
-    pattern_to_re(&out)
-}
-
-fn binomial_re2() -> RecExpr<RiseENode> {
-    let nbhL = 0;
-    let nbhH = 1;
-    let nbhV = 2;
-
-    let out = map2(
-        lam(nbhL, map2(
-            lam(nbhH, dot2(symb("weightsH"), var(nbhH))),
-            slide3(num(3), num(1), map2(lam(nbhV, dot2(symb("weightsV"), var(nbhV))), transpose1(var(nbhL))))
-        )),
-        slide3(num(3), num(1), symb("input"))
-    );
-
-    pattern_to_re(&out)
-}
-
 #[test]
-#[ignore]
 pub fn test_binomial() {
-    assert_reaches(binomial_re1(), binomial_re2(), 40);
+    let a = "(app (app sym_map (app sym_map (lam s0 (app (app (app sym_reduce sym_add) num_0) (app (app sym_map (lam s-1 (app (app sym_mul (app sym_fst (var s-1))) (app sym_snd (var s-1))))) (app (app sym_zip (app sym_join sym_weights2d)) (app sym_join (var s0)))))))) (app (app sym_map sym_transpose) (app (app (app sym_slide num_3) num_1) (app (app sym_map (app (app sym_slide num_3) num_1)) sym_input))))";
+    let b = "(app (app sym_map (lam s0 (app (app sym_map (lam s1 (app (app (app sym_reduce sym_add) num_0) (app (app sym_map (lam s-2 (app (app sym_mul (app sym_fst (var s-2))) (app sym_snd (var s-2))))) (app (app sym_zip sym_weightsH) (var s1)))))) (app (app (app sym_slide num_3) num_1) (app (app sym_map (lam s2 (app (app (app sym_reduce sym_add) num_0) (app (app sym_map (lam s-3 (app (app sym_mul (app sym_fst (var s-3))) (app sym_snd (var s-3))))) (app (app sym_zip sym_weightsV) (var s2)))))) (app sym_transpose (var s0))))))) (app (app (app sym_slide num_3) num_1) sym_input))";
+    assert_reaches(a, b, 40);
 }
