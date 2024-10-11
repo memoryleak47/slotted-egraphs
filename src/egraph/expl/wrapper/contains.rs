be9c4915c7e6e5f4c0ca80b7bb223c68064bc8de
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-11 11:58:50 +0200

    start setting up ProvenContains

--- src/egraph/expl/wrapper/contains.rs
+++ src/egraph/expl/wrapper/contains.rs
@@ -1,6 +1,5 @@
 use crate::*;
 
-// Should ProvenNode also contain the src-id?
 pub struct ProvenContains<L> {
     // contains the proof that the e-node is equal to some app-ids syn-enode.
     pub elem: ProvenNode<L>,
@@ -8,4 +7,36 @@ pub struct ProvenContains<L> {
     // proofs that this app-id is equal to our target app-id.
     #[cfg(feature = "explanations")]
     pub proof: ProvenEq,
+}
+
+impl<L: Language> EGraph<L> {
+    pub fn refl_pc(&self, i: Id) -> ProvenContains<L> {
+        let id = self.mk_syn_identity_applied_id(i);
+        let node = self.get_syn_node(&id);
+        ProvenContains {
+            elem: self.refl_pn(&node),
+            #[cfg(feature = "explanations")]
+            proof: prove_reflexivity(&id, &self.proof_registry),
+        }
+    }
+
+    pub fn chain_pc_map(&self, start: &ProvenContains<L>, f: impl Fn(usize, ProvenAppliedId) -> ProvenAppliedId) -> ProvenContains<L> {
+        ProvenContains {
+            elem: self.chain_pn_map(&start.elem, f),
+            #[cfg(feature = "explanations")]
+            proof: start.proof.clone(),
+        }
+    }
+
+    pub fn chain_pc_eq(&self, start: &ProvenContains<L>, eq: ProvenEq) -> ProvenContains<L> {
+        ProvenContains {
+            elem: start.elem.clone(),
+            #[cfg(feature = "explanations")]
+            proof: prove_transitivity(start.proof.clone(), eq.clone(), &self.proof_registry),
+        }
+    }
+
+    pub fn pc_congruence(&self, a: &ProvenContains<L>, b: &ProvenContains<L>) -> ProvenEq {
+        todo!()
+    }
 }
