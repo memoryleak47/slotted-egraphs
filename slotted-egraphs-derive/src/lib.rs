3c2a0a00064c8ac014664e4f7d5475f6e1d0ebe0
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-19 17:33:20 +0100

    ...

--- slotted-egraphs-derive/src/lib.rs
+++ slotted-egraphs-derive/src/lib.rs
@@ -3,15 +3,21 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote, ToTokens};
 use syn::*;
 
+// We allow the user to use tuples, Slot, Bind<_>, AppliedId and "user-defined types" in their enum variants.
+// user-defined types will be understood as slot-independent constants, and ignored by the system.
+
 #[proc_macro]
 pub fn define_language(input: TokenStream1) -> TokenStream1 {
     let mut ie: ItemEnum = parse(input).unwrap();
 
     let name = ie.ident.clone();
-    let names: Vec<Expr> = ie.variants.iter_mut()
+    let str_names: Vec<Expr> = ie.variants.iter_mut()
                                       .map(|x| {
                                           x.discriminant.take().expect("Name missing!").1
                                       }).collect();
+
+    let ident_names: Vec<Ident> = ie.variants.iter().map(|x| x.ident.clone()).collect();
+
     quote! {
         #[derive(PartialEq, Eq, Hash, Clone, Debug)]
         #ie
@@ -20,8 +26,19 @@ pub fn define_language(input: TokenStream1) -> TokenStream1 {
             fn all_slot_occurences_mut(&mut self) -> Vec<&mut Slot> { todo!() }
             fn public_slot_occurences_mut(&mut self) -> Vec<&mut Slot> { todo!() }
             fn applied_id_occurences_mut(&mut self) -> Vec<&mut AppliedId> { todo!() }
-            fn to_op(&self) -> (String, Vec<Child>) { todo!() }
-            fn from_op(op: &str, children: Vec<Child>) -> Option<Self> { todo!() }
+
+            fn to_op(&self) -> (String, Vec<Child>) {
+                match self {
+                    #(#name::#ident_names(..) => { (#str_names.to_string(), todo!()) },)*
+                }
+            }
+
+            fn from_op(op: &str, children: Vec<Child>) -> Option<Self> {
+                match op {
+                    #(#str_names => { Some(todo!()/* #name::#ident_names */) },)*
+                    _ => None,
+                }
+            }
         }
     }.to_token_stream().into()
 }
