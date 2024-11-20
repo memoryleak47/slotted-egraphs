fee11d27be57b9c11d7710ada1a8ecb80a977144
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-20 16:34:04 +0100

    impl to_op in proc macro

--- slotted-egraphs-derive/src/lib.rs
+++ slotted-egraphs-derive/src/lib.rs
@@ -11,15 +11,16 @@ pub fn define_language(input: TokenStream1) -> TokenStream1 {
     let mut ie: ItemEnum = parse(input).unwrap();
 
     let name = ie.ident.clone();
-    let str_names: Vec<Expr> = ie.variants.iter_mut()
+    let str_names: Vec<Option<Expr>> = ie.variants.iter_mut()
                                       .map(|x| {
-                                          x.discriminant.take().expect("Name missing!").1
+                                          x.discriminant.take().map(|(_, e)| e)
                                       }).collect();
 
     let ident_names: Vec<Ident> = ie.variants.iter().map(|x| x.ident.clone()).collect();
     let all_slot_occurences_mut_arms: Vec<TokenStream2> = ie.variants.iter().map(|x| produce_all_slot_occurences_mut(&name, x)).collect();
     let public_slot_occurences_mut_arms: Vec<TokenStream2> = ie.variants.iter().map(|x| produce_public_slot_occurences_mut(&name, x)).collect();
     let applied_id_occurences_mut_arms: Vec<TokenStream2> = ie.variants.iter().map(|x| produce_applied_id_occurences_mut(&name, x)).collect();
+    let to_op_arms: Vec<TokenStream2> = ie.variants.iter().zip(&str_names).map(|(x, n)| produce_to_op(&name, &n, x)).collect();
 
     quote! {
         #[derive(PartialEq, Eq, Hash, Clone, Debug)]
@@ -44,13 +45,12 @@ pub fn define_language(input: TokenStream1) -> TokenStream1 {
 
             fn to_op(&self) -> (String, Vec<Child>) {
                 match self {
-                    #(#name::#ident_names(..) => { (#str_names.to_string(), todo!()) },)*
+                    #(#to_op_arms),*
                 }
             }
 
             fn from_op(op: &str, children: Vec<Child>) -> Option<Self> {
                 match op {
-                    #(#str_names => { Some(todo!()/* #name::#ident_names */) },)*
                     _ => None,
                 }
             }
@@ -101,6 +101,24 @@ fn produce_applied_id_occurences_mut(name: &Ident, v: &Variant) -> TokenStream2 
                 out.extend(#fields .applied_id_occurences_mut());
             )*
             out
+        }
+    }
+}
+
+fn produce_to_op(name: &Ident, e: &Option<Expr>, v: &Variant) -> TokenStream2 {
+    let e = e.as_ref().unwrap();
+    let variant_name = &v.ident;
+    let n = v.fields.len();
+    let fields: Vec<Ident> = (0..n).map(|x| Ident::new(&format!("a{x}"), proc_macro2::Span::call_site())).collect();
+    quote! {
+        #name::#variant_name(#(#fields),*) => {
+            let mut out: Vec<Child> = Vec::new();
+            #(
+                let (l, ch) = #fields.to_op();
+                assert!(l == "");
+                out.extend(ch);
+            )*
+            (String::from(#e), out)
         }
     }
 }
