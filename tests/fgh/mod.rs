90233713e066724534927eca9675b82128daf361
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-20 17:41:14 +0100

    use define_language! for fgh

--- tests/fgh/mod.rs
+++ tests/fgh/mod.rs
@@ -4,49 +4,11 @@
 use slotted_egraphs::*;
 use crate::*;
 
-#[derive(Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Debug)]
-pub enum Fgh {
-    F(Slot, Slot),
-    G(Slot, Slot),
-    H(Slot, Slot),
-}
-
-impl Language for Fgh {
-    fn all_slot_occurences_mut(&mut self) -> Vec<&mut Slot> {
-        match self {
-            Fgh::F(x, y) => vec![x, y],
-            Fgh::G(x, y) => vec![x, y],
-            Fgh::H(x, y) => vec![x, y],
-        }
-    }
-
-    fn public_slot_occurences_mut(&mut self) -> Vec<&mut Slot> {
-        match self {
-            Fgh::F(x, y) => vec![x, y],
-            Fgh::G(x, y) => vec![x, y],
-            Fgh::H(x, y) => vec![x, y],
-        }
-    }
-
-    fn applied_id_occurences_mut(&mut self) -> Vec<&mut AppliedId> {
-        vec![]
-    }
-
-    fn to_op(&self) -> (String, Vec<Child>) {
-        match self.clone() {
-            Fgh::F(x, y) => (String::from("f"), vec![Child::Slot(x), Child::Slot(y)]),
-            Fgh::G(x, y) => (String::from("g"), vec![Child::Slot(x), Child::Slot(y)]),
-            Fgh::H(x, y) => (String::from("h"), vec![Child::Slot(x), Child::Slot(y)]),
-        }
-    }
-
-    fn from_op(op: &str, children: Vec<Child>) -> Option<Self> {
-        match (op, &*children) {
-            ("f", [Child::Slot(x), Child::Slot(y)]) => Some(Fgh::F(*x, *y)),
-            ("g", [Child::Slot(x), Child::Slot(y)]) => Some(Fgh::G(*x, *y)),
-            ("h", [Child::Slot(x), Child::Slot(y)]) => Some(Fgh::H(*x, *y)),
-            _ => None,
-        }
+define_language! {
+    pub enum Fgh {
+        F(Slot, Slot) = "f",
+        G(Slot, Slot) = "g",
+        H(Slot, Slot) = "h",
     }
 }
 
