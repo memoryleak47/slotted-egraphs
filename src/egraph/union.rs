6ffb93d79d3c29bb296894ba28f097cc7216daec
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-25 21:24:56 +0100

    ...

--- src/egraph/union.rs
+++ src/egraph/union.rs
@@ -26,8 +26,13 @@ impl<L: Language, N: Analysis<L>> EGraph<L, N> {
         let proof = ghost!(self.prove_explicit(&syn_a, &syn_b, justification));
 
         let out = self.union_internal(&a, &b, proof);
-        self.rebuild();
+        self.rebuild_called_from_union_instantiations();
         out
+    }
+
+    #[cfg_attr(feature = "trace", instrument(level = "trace", skip_all))]
+    fn rebuild_called_from_union_instantiations(&mut self) {
+        self.rebuild();
     }
 
     #[cfg_attr(feature = "trace", instrument(level = "trace", skip_all))]
