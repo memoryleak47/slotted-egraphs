30e7e169439b2c08e876404898fdd7652c24da8c
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-27 17:24:18 +0100

    add base Language

--- src/lang.rs
+++ src/lang.rs
@@ -2,13 +2,35 @@ use crate::*;
 
 pub trait Language {
     type With<R>;
+
+    fn with_clone<R: Clone>(x: &Self::With<R>) -> Self::With<R>;
 }
 
-enum MyLang<R=AppliedId> {
+pub struct Term<L: Language>(L::With<Box<Term<L>>>);
+
+impl<L: Language> Clone for Term<L> {
+    fn clone(&self) -> Self {
+        Term(L::with_clone(&self.0))
+    }
+}
+
+
+/// Example:
+struct MyLang;
+
+#[derive(Clone)]
+enum MyNode<R> {
     Var(u32),
     App(R, R),
 }
 
 impl Language for MyLang {
-    type With<R2> = MyLang<R2>;
+    type With<R2> = MyNode<R2>;
+
+    fn with_clone<R: Clone>(x: &Self::With<R>) -> Self::With<R> { x.clone() }
+}
+
+fn foo() {
+    let x: Term<MyLang> = Term(MyNode::Var(20));
+    let y = x.clone();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,13 @@
 use crate::*;
 
+#[derive(Clone)]
 pub struct AppliedId(SlotMap, Id);
 
+#[derive(Clone)]
 pub struct SlotMap(Vec<(Slot, Slot)>);
 
+#[derive(Clone)]
 pub struct Slot(usize);
+
+#[derive(Clone)]
 pub struct Id(usize);
