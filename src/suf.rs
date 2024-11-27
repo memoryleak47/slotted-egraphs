11e5c39cf347ca84780e7dcb3824f8e6cdfef81c
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-27 17:30:10 +0100

    ...

--- src/suf.rs
+++ src/suf.rs
@@ -1,11 +1,20 @@
 use crate::*;
 
-pub struct SlottedUF(Vec<SUFClass>);
-
-struct SUFClass {
-    leader: AppliedId,
-    s: Set<Slot>,
-    g: Group,
+trait Annotation {
+    fn apply_slotmap(m: &SlotMap) -> Self;
+    fn compose(&self, other: &Self) -> Self;
+    fn inverse(&self) -> Self;
+    fn refl(x: AppliedId) -> Self;
 }
 
-struct Group;
+pub struct SlottedUF<A: Annotation>(Vec<SUFClass<A>>);
+
+struct SUFClass<A: Annotation> {
+    leader: AppliedId,
+    annotation_to_leader: A,
+
+    s: Set<Slot>,
+    g: Group<A>,
+}
+
+struct Group<A: Annotation>(A);
