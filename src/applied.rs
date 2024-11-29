4339cba90eeb64dc630a6311075da8ece2b585b8
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-29 13:11:36 +0100

    ...

--- src/applied.rs
+++ src/applied.rs
@@ -16,23 +16,23 @@ pub trait Applicable {
 
     fn apply_slotmap(&self, m: &SlotMap) -> Self where Self: Clone {
         let mut c = self.clone();
-        c.apply_slotmap_mut(m);
+        c.apply_slotmap_inplace(m);
         c
     }
 
     fn apply_slotmap_fresh(&self, m: &SlotMap) -> Self where Self: Clone {
         let mut c = self.clone();
-        c.apply_slotmap_fresh_mut(m);
+        c.apply_slotmap_fresh_inplace(m);
         c
     }
 
-    fn apply_slotmap_mut(&mut self, m: &SlotMap) {
+    fn apply_slotmap_inplace(&mut self, m: &SlotMap) {
         for x in self.access_slots_mut() {
             *x = m[*x];
         }
     }
 
-    fn apply_slotmap_fresh_mut(&mut self, m: &SlotMap) {
+    fn apply_slotmap_fresh_inplace(&mut self, m: &SlotMap) {
         let mut m = m.clone();
         for x in self.access_slots_mut() {
             if let Some(y) = m.get(*x) {
@@ -75,7 +75,7 @@ impl Applicable for SlotMap {
 
 impl<T: Applicable> Applied<T> {
     fn apply(mut self) -> T {
-        self.t.apply_slotmap_mut(&self.m);
+        self.t.apply_slotmap_inplace(&self.m);
         self.t
     }
 }
@@ -84,7 +84,7 @@ impl<T: Applicable> Mul<T> for SlotMap {
     type Output = T;
 
     fn mul(self, mut t: T) -> T {
-        t.apply_slotmap_mut(&self);
+        t.apply_slotmap_inplace(&self);
         t
     }
 }
