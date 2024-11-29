7d27dd089e67cddfa65ace45ea7df92e767d9721
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-29 20:45:54 +0100

    ...

--- src/segraph/core.rs
+++ src/segraph/core.rs
@@ -1,20 +1,28 @@
 use crate::*;
 
 trait Component<L: Language> {
-    fn construct(l: &Node<L>) -> Self;
+    fn construct(l: &Shape<L>) -> Self;
 }
 
 struct SEClass<L: Language, C: Component<L>> {
-    nodes: HashMap<Node<L>, SlotMap>,
+    nodes: HashMap<Shape<L>, SlotMap>,
     c: C,
 }
 
 // The API of this module enforces that `hashcons`, `usages` and `nodes` agree with each other.
 // Thus it also enforces that every shape exists uniquely, due to the hashcons.
+//
+// invariants:
+// - hashcons[sh] represents sh
+// - i represents classes[i].nodes[sh] * sh
+// - usages[i].contains(sh) <-> sh in hashcons && sh references i
+//
+// Note: the bijection in the hashcons goes from Id    to Shape,
+// while the bijection in the nodes    goes from Shape to Id.
 pub struct SEGraph<L: Language, C: Component<L>> {
     suf: SUF<SEClass<L, C>>,
-    hashcons: HashMap<Node<L>, AppliedId>,
-    usages: HashMap<Id, HashSet<Node<L>>>,
+    hashcons: HashMap<Shape<L>, AppliedId>,
+    usages: HashMap<Id, HashSet<Shape<L>>>,
 }
 
 impl<L: Language, C: Component<L>> SEGraph<L, C> {
@@ -29,14 +37,14 @@ impl<L: Language, C: Component<L>> SEGraph<L, C> {
     // These functions do not do shape computation! It needs to be done prior.
 
     // if the shape already exists, reject it, and return the collision.
-    pub(in crate::segraph) fn add_shape(&mut self, sh: Shape<L>, a: AppliedId) -> Option<(Node<L>, AppliedId, AppliedId)> where Shape<L>: Hash + Eq + Clone {
+    pub(in crate::segraph) fn add_shape(&mut self, sh: Shape<L>, x: AppliedId) -> Option<(Shape<L>, AppliedId, AppliedId)> where Shape<L>: Hash + Eq + Clone {
         match self.hashcons.entry(sh) {
             Entry::Occupied(e) => {
-                Some((e.key().clone(), e.get().clone(), a))
+                Some((e.key().clone(), e.get().clone(), x))
             },
             Entry::Vacant(e) => {
                 // TODO update usages and nodes.
-                e.insert(a);
+                e.insert(x);
                 None
             },
         }
