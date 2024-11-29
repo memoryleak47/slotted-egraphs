6307d7df3b90fbeeb0b1485d5e62a10f36b48cb8
memoryleak47 <memoryleak47@protonmail.ch>
2024-11-29 13:25:43 +0100

    ...

--- src/suf.rs
+++ src/suf.rs
@@ -9,3 +9,21 @@ struct SUFClass<C> {
 
     c: C,
 }
+
+impl<C> SlottedUF<C> {
+    pub fn new() -> Self {
+        SlottedUF(Vec::new())
+    }
+
+    pub fn add(&mut self, s: HashSet<Slot>, c: C) -> AppliedId {
+        todo!()
+    }
+
+    pub fn union(&mut self, x: &AppliedId, y: &AppliedId) {
+        todo!()
+    }
+
+    pub fn is_equal(&self, x: &AppliedId, y: &AppliedId) {
+        todo!()
+    }
+}
