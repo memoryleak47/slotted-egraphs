214152a62ee61d9d02931fa1dbbd1a1fadbf570d
a0+1@berkeley.edu <mj9012159@gmail.com>
2025-03-11 13:02:43 +0100

    added comments to assert_reaches

--- tests/entry.rs
+++ tests/entry.rs
@@ -100,6 +100,7 @@ where
     });
 }
 
+// assert that `start` is in the same e-class as `goal` in `steps` steps.
 fn assert_reaches<L, N>(start: &str, goal: &str, rewrites: &[Rewrite<L, N>], steps: usize)
 where
     L: Language + 'static,
@@ -117,6 +118,7 @@ where
     let report = runner.run(rewrites);
 
     if let StopReason::Other(_) = report.stop_reason {
+        // `start` did not reach `goal` in `steps` steps.
         runner.egraph.dump();
         assert!(false);
     }
