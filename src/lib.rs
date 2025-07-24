662ecf9ff00977a566b939e67f502622e6656ca7
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-24 09:42:18 +0100

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ struct Unionfind {
 struct Class {
     leader: AppliedId,
     group: Group,
+    arity: usize, // for now. will probably be stored somewhere else later.
 }
 
 struct AppliedId {
@@ -14,7 +15,6 @@ struct AppliedId {
     m: SlotMap,
 }
 
-// SlotMap m maps x to m.0[x]
 struct SlotMap(Box<[Slot]>);
 
 struct Id(usize);
@@ -22,3 +22,23 @@ struct Id(usize);
 struct Group(HashSet<SlotMap>);
 
 struct Slot(u32);
+
+impl Unionfind {
+    pub fn add(&mut self, arity: usize) -> Id { // should this return AppliedId?
+        let i = Id(self.classes.len());
+        self.classes.push(Class {
+            leader: todo!(),
+            group: Group(HashSet::new()),
+            arity,
+        });
+        i
+    }
+
+    pub fn union(&mut self, x: &AppliedId, y: &AppliedId) {
+        todo!()
+    }
+
+    pub fn eq(&self, x: &AppliedId, y: &AppliedId) -> bool {
+        todo!()
+    }
+}
