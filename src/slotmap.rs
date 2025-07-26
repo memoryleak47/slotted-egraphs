70fc6d8f625444a7fc4b47a3042c4e73f46b9572
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-26 12:13:27 +0100

    ...

--- src/slotmap.rs
+++ src/slotmap.rs
@@ -1,23 +1,7 @@
 use std::ops::Index;
 
-// Types of slotmaps:
-// - unionfind slotmaps: equating different Ids.
-// -- redundant slots possible! This corresponds to "holes" in the slotmap.
-// -- But only in one direction! the unionfind only gets more redundant the further you go.
-//
-// - group slotmaps: expressing symmetries
-// -- (keyset = valueset, perfect for positional perms)
-//
-// - shape slotmaps: expressing how shape slots relate to e-class slots.
-// -- redundant slots are again a thing! But again, the class always has less (<=) slots than the shape.
-//
-// Exceptions:
-// - When the user calls "union", we can introduce redundant slots on both sides, so no (<=) guarantee anymore.
-// -- technically, if we give it two AppliedIds, we could get the (<=) back.
-
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
-// slotmap m maps x to m.0[x.0] if it's not Slot::missing
-// we require that a slotmap m has m.0 is a reordering of 0..m.len()
+// slotmap m, has m[x] = m.0[x.0], we return Slot::missing for unmapped slots.
 pub struct SlotMap(pub Box<[Slot]>);
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
@@ -29,6 +13,7 @@ impl SlotMap {
         SlotMap(v)
     }
 
+    // corresponds to (self * other)[x] = self[other[x]], just like in the paper.
     pub fn compose(&self, other: &SlotMap) -> SlotMap {
         todo!()
     }
