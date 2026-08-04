970183c868108314bb68ef9c4fb97fd9145c6f37
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-04 18:21:34 +0200

    document redundancy_matching_bug

--- tests/lambda/mod.rs
+++ tests/lambda/mod.rs
@@ -40,3 +40,26 @@ define_language! {
         Let(Bind<AppliedId>, AppliedId) = "let",
     }
 }
+
+#[test]
+fn redundancy_matching_bug() {
+    let mut eg: EGraph<Lambda> = EGraph::new(());
+    let t = RecExpr::parse("(app (lam $x (var $x)) (lam $x (var $x)))").unwrap();
+    eg.add_expr(t);
+
+    assert_eq!(eg.total_number_of_nodes(), 3);
+
+    let r: Rewrite<Lambda> = rw!("compose_identity"; "(app (lam $x (var $x)) (lam $x (var $x)))" => "(lam $x (var $x))");
+    apply_rewrites(&mut eg, &[r]);
+
+    eg.dump();
+
+    // NOTE: this is a bug. The rule `compose_identity` didn't fire even though it precisely matches the added term `t`.
+    // The reason for that is that during e-matching
+    // (app (lam $x (var $x)) (lam $x (var $x)))
+    // gets rewritten to
+    // (app (lam $x1 (var $x1)) (lam $x2 (var $x2)))
+    // to avoid naming collisions.
+
+    assert_eq!(eg.total_number_of_nodes(), 2);
+}
