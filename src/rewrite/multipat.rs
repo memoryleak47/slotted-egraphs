c4d2e094247eef86fb81c0e1653ec537b062a7d3
memoryleak47 <memoryleak47@protonmail.ch>
2026-08-07 16:06:33 +0200

    start multipat matching

--- src/rewrite/multipat.rs
+++ src/rewrite/multipat.rs
@@ -12,6 +12,52 @@ pub struct MultiPattern<L: Language> {
     pats: Vec<(PVar, L, Vec<PVar>)>,
 }
 
+#[derive(Clone)]
+struct MultiState {
+    diseq_constraints: HashMap<Slot, Vec<Slot>>,
+    subst: Subst,
+}
+
 pub fn multi_ematch<L: Language>(pat: &MultiPattern<L>, eg: &EGraph<L>) -> Vec<Subst> {
-    todo!()
+    let mut states: Vec<MultiState> = vec![MultiState {
+        diseq_constraints: HashMap::default(),
+        subst: Subst::default(),
+    }];
+
+    for (v, n, ch) in &pat.pats {
+        for st in std::mem::take(&mut states) {
+            states.extend(multi_ematch_step(v, n, ch, st, eg));
+        }
+    }
+
+    // TODO final_subst?
+    states.into_iter().map(|x| x.subst).collect()
+}
+
+fn multi_ematch_step<L: Language>(pv: &PVar, node: &L, children: &[PVar], mut state: MultiState, eg: &EGraph<L>) -> Vec<MultiState> {
+    let mut out = Vec::new();
+    for state in multi_ematch_step_class(pv, node, children, state, eg) {
+        out.extend(multi_ematch_step_node(pv, node, children, state, eg));
+    }
+    out
+}
+
+
+fn multi_ematch_step_class<L: Language>(pv: &PVar, node: &L, children: &[PVar], mut state: MultiState, eg: &EGraph<L>) -> Vec<MultiState> {
+    if state.subst.contains_key(pv) { return vec![state] }
+
+    let mut out = Vec::new();
+    for x in eg.ids() {
+        let slots = &eg.slots(x);
+        let m = SlotMap::bijection_from_fresh_to(&slots).inverse();
+        let mut state = state.clone();
+        state.subst.insert(pv.clone(), AppliedId::new(x, m));
+        out.push(state);
+    }
+    out
+}
+
+fn multi_ematch_step_node<L: Language>(pv: &PVar, node: &L, children: &[PVar], mut state: MultiState, eg: &EGraph<L>) -> Vec<MultiState> {
+    // TODO
+    vec![state]
 }
