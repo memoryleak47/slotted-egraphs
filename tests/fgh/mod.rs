46b8d447f1d519be16a13af275b3f0c520a05a78
memoryleak47 <memoryleak47@protonmail.ch>
2026-09-01 13:55:50 +0200

    add variant of next bug found by oflatt-claude

--- tests/fgh/mod.rs
+++ tests/fgh/mod.rs
@@ -20,3 +20,20 @@ fn transitive_symmetry() {
     eg.dump();
     explain("(f $1 $2)", "(h $2 $1)", eg);
 }
+
+// bug found by oflatt-claude.
+#[test]
+fn shrink_with_symmetry() {
+    let eg: &mut EGraph<Fgh> = &mut EGraph::default();
+    equate("(f $0 $1)", "(f $1 $0)", eg);
+    equate("(f $0 $1)", "(g $1 $1)", eg);
+
+    let ids = eg.ids();
+    eg.dump();
+    assert_eq!(ids.len(), 1);
+
+    // here I disagree with claude.
+    // There should be 0 slots, instead of 1.
+    // After all, as (f $0 $1) is self-symmetric, making $0 redundant should make $1 redundant too.
+    assert_eq!(eg.slots(ids[0]).len(), 0);
+}
